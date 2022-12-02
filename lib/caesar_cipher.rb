200a4069d6905fd8a55e28464835dd0808d7190c
Cheong <xxlsxx1955@gmail.com>
2022-12-02 20:13:54 +0800

    Add tests

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: false
 
-def lowercase_letter?(char)
+def lowercase_char?(char)
   char.ord.between?(97, 122)
 end
 
-def uppercase_letter?(char)
+def uppercase_char?(char)
   char.ord.between?(65, 90)
 end
 
@@ -12,15 +12,17 @@ def caesar_cipher(string, shift_factor)
   lowercase_shifted_letters = ('a'..'z').to_a.rotate(shift_factor)
   uppercase_shifted_letters = ('A'..'Z').to_a.rotate(shift_factor)
 
-  result = string.split('').map do |letter|
-    if lowercase_letter?(letter)
-      index = (letter.ord - 97) % 26
+  result = string.chars.map do |char|
+    if lowercase_char?(char)
+      index = (char.ord - 97) % 26
+
       lowercase_shifted_letters[index]
-    elsif uppercase_letter?(letter)
-      index = (letter.ord - 65) % 26
+    elsif uppercase_char?(char)
+      index = (char.ord - 65) % 26
+
       uppercase_shifted_letters[index]
     else
-      raise 'The character is not a string'
+      char
     end
   end
 
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,3 +1,19 @@
 # frozen_string_literal: false
 
 require_relative './../lib/caesar_cipher.rb'
+
+describe '#caesar_cipher' do
+  context 'given a string and a shift factor' do
+    it 'modify the string according to shift factor' do
+      expect(caesar_cipher('What a string!', 5)).to eq('Bmfy f xywnsl!')
+    end
+
+    it 'does not modify the string if shift factor is zero' do
+      expect(caesar_cipher('What a string!', 0)).to eq('What a string!')
+    end
+
+    it 'only modify letter' do
+      expect(caesar_cipher('!!!@@@###$$$123', 10)).to eq('!!!@@@###$$$123')
+    end
+  end
+end
